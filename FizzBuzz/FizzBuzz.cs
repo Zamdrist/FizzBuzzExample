f4c2c4f9bf09c62f6d6bb3b6cc7637d181095aae
Steve <zamdrist@gmail.com>
2018-06-18 22:23:21 -0500

    First commit

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,33 @@
+﻿namespace FizzBuzzExample
+{
+    public class FizzBuzz
+    {
+	    public string FizzBuzzOutput { get; }
+
+	    public FizzBuzz()
+	    {
+		    var fizzBuzz = string.Empty;
+		    for (var i = 0; i <= 100; i++)
+		    {
+			    if (i % 15 == 0)
+			    {
+				    fizzBuzz += "FizzBuzz\n";
+			    }
+				else if (i % 5 == 0)
+			    {
+				    fizzBuzz += "Buzz\n";
+
+			    }
+				else if (i % 3 == 0)
+			    {
+				    fizzBuzz += "Fizz\n";
+			    }
+			    else
+			    {
+				    fizzBuzz += i + "\n";
+			    }
+		    }
+		    this.FizzBuzzOutput = fizzBuzz;
+	    }
+    }
+}
---
+++ RunFizzBuzz/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FizzBuzzExample;
+
+namespace RunFizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        var fb = new FizzBuzz();
+	        Console.WriteLine(fb.FizzBuzzOutput);
+	        Console.ReadLine();
+        }
+    }
+}
